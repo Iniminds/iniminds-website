005b1c039c31927dd68cbd1a850ac2c700c8c326
Archit Trivedi <archittrivediui@gmail.com>
2016-01-16 23:47:31 -0800

    contact.php and feedback.php added.

---
+++ contact.php
@@ -0,0 +1,222 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>iMinds Contact</title>
+<script src="SpryMenuBar.js" type="text/javascript"></script>
+<link href="SpryMenuBarHorizontal.css" rel="stylesheet" type="text/css" />
+<style type="text/css">
+<!--
+.style3 {
+	color: #000000;
+	font-weight: bold;
+	font-size: 24px;
+}
+.style5 {color: #999999}
+.style6 {font-weight: bold}
+.style19 {color: #000066}
+.style21 {color: #0000FF; font-size: 24;}
+.style23 {	font-size: 20px;
+	font-weight: bold;
+}
+.style24 {	font-weight: bold;
+	font-size: 22px;
+}
+.style25 {	color: #0000FF;
+	font-weight: bold;
+}
+.style26 {color: #CCCCCC; font-weight: bold; }
+-->
+</style>
+<script type="text/javascript">
+function get()
+{
+	document.getElementById('q').value=' ';
+}
+</script>
+</head>
+<body background="images/img07.jpg">
+<form action="http://www.google.com/search" target="_blank" method="get"> 
+<table width="1184" border="0" align="center"  bgcolor="#999999">
+  <tr>
+    <td height="225" colspan="8" ><img src="headerimg.jpg"  height="106%" width="100%"/>           </td>
+  </tr>
+  <tr>
+  <td height="25" colspan="2"  background="menuimg.png"> 
+  <ul id="MenuBar1" class="MenuBarHorizontal">
+  <li><a class="MenuBarItemSubmenu" href="index.php">HOME</a>
+  <li><a class="MenuBarItemSubmenu" href="#">REFERENCES</a>
+      <ul>
+        <li><a href="c.php">C/C++</a></li>
+        <li><a href="java.php">JAVA</a></li>
+        <li><a href="python.php">PYTHON</a></li>
+        <li><a href="linux.php">LINUX</a></li>
+      </ul>
+  </li>
+<li><a class="MenuBarItemSubmenu" href="#">OFFTOPIC</a>
+            <ul>
+              <li><a href="tech.php">TECH</a></li>
+              <li><a href="nontech.php">NON-TECH</a></li>
+            </ul>
+        
+         
+      <li><a class="MenuBarItemSubmenu" href="genesis.php">GENESIS</a>
+           <li><a class="MenuBarItemSubmenu" href="feedback.php">FEEDBACK</a></li>
+       <li><a href="#" class="MenuBarItemSubmenu style6">ABOUT US</a>
+       <ul>
+        <li><a href="members.php">MEMBERS</a></li>
+        <li><a href="contact.php">CONTACT</a></li>
+      </ul>
+</ul>
+
+  <div align="right">
+    <script type="text/javascript">
+<!--
+var MenuBar1 = new Spry.Widget.MenuBar("MenuBar1", {imgDown:"SpryAssets/SpryMenuBarDownHover.gif", imgRight:"SpryAssets/SpryMenuBarRightHover.gif"});
+//-->
+  </script>
+    
+    
+    
+    
+    
+    <input type="text"  name="q" id="q" style="width: 120px; color:#999999" onFocus="get();" value="Google" />
+    <input type="submit" value="Search" /> 
+  </div></td>
+  </tr>
+  <tr   bordercolor="#FFFFDF" >
+    <td width="954" rowspan="4" bgcolor="#ECECFF" valign="top">
+    <br><br>
+    <table width="757" border="0">
+      <tr>
+        <td width="157">Aashir Sutar</td>
+        <td width="198">aaraha@gmail.com</td>
+        <td width="205">www.twitter.com/aashirS</td>
+        <td width="179"><div align="center">-</div></td>
+      </tr>
+      <tr>
+        <td>Archit Trivedi</td>
+        <td>aarchittrivedi13@gmail.com</td>
+        <td>www.twitter.com/archit13</td>
+        <td><div align="center">+1 9729003058</div></td>
+      </tr>
+      <tr>
+        <td>Arwa Kuterwadliwala</td>
+        <td>arwa.rocks16@gmail.com</td>
+        <td>www.twitter.com/arwa16</td>
+        <td><div align="center">-</div></td>
+      </tr>
+      <tr>
+        <td>Bhargav Dabhi</td>
+        <td>only4ubhar@gmail.com</td>
+        <td>www.twitter.com/bhargi96</td>
+        <td><div align="center">+91 9428659794</div></td>
+      </tr>
+      <tr>
+        <td>Haresh Vaviya</td>
+        <td>vaviyaharesh@gmail.com</td>
+        <td>www.twitter.com/haresh_vaviya</td>
+        <td><div align="center">+91 9925199308</div></td>
+      </tr>
+      <tr>
+        <td>Jaydeep Gajera</td>
+        <td>jaydeepgajera90@gmail.com</td>
+        <td>www.twitter.com/gajera90</td>
+        <td><div align="center">+91 9033378964</div></td>
+      </tr>
+      <tr>
+        <td>Nisarg Patel</td>
+        <td>nisarg246005@gmail.com</td>
+        <td>www.twitter.com/nisarg1792</td>
+        <td><div align="center">+91 9033779487 </div></td>
+      </tr>
+      <tr>
+        <td>Prithvi Patel</td>
+        <td>patelprithvi07@gmail.com </td>
+        <td>www.twitter.com/prithvi_patel</td>
+        <td><div align="center">-</div></td>
+      </tr>
+      <tr>
+        <td>Rahul Der</td>
+        <td>der.rahul@gmail.com</td>
+        <td>www.twitter.com/der43</td>
+        <td><div align="center">+91 9998555008</div></td>
+      </tr>
+      <tr>
+        <td>Rushi Modi</td>
+        <td>rushi_2591@yahoo.co.in</td>
+        <td>www.twitter.com/rushi2591</td>
+        <td><div align="center">+91 9662543283</div></td>
+      </tr>
+      <tr>
+        <td>Shikha Arora</td>
+        <td>shikhaarora7791@gmail.com</td>
+        <td>www.twitter.com/shikha_7</td>
+        <td><div align="center">-</div></td>
+      </tr>
+      <tr>
+        <td>Sohil Dhol</td>
+        <td>dholsohil@gmail.com</td>
+        <td>www.twitter.com/dholsohil</td>
+        <td><div align="center">+91 9714481177</div></td>
+      </tr>
+      <tr>
+        <td>Sonal Patel</td>
+        <td>sonalpatel824@yahoo.com</td>
+        <td>www.twitter.com/sonal824</td>
+        <td><div align="center">-</div></td>
+      </tr>
+      <tr>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td><div align="center"></div></td>
+      </tr>
+    </table></td>
+    <td width="220"  height="25" background="box_h3.png"><div align="center"><span class="style26">Recent Updates</span> </div></td>
+  </tr>
+  <tr>
+    <td height="213" background="Untitled - Copy.png"><marquee  onmouseover="this.stop()" onmouseout="this.start()" scrollamount="2" scrolldelay="0" direction="up" >
+    <div align="justify">
+      <p class="style19"><img src="new.gif" alt="new1" /> Python Workshop @ ADIT organized by Computer Engg Department under ISTE Student Chapter. <br />
+        <br />
+        <img src="new.gif" alt="new" />Solved C/C++ &amp; JAVA Programs. </p>
+      <p align="left" class="style19"><img src="new.gif" alt="new" /> Coming Soon:-UML.SQLqueries and more programs</p>
+    </div>
+    </marquee></td>
+  </tr>
+  <tr>
+    <td height="25"  background="box_h3.png"><div align="center"><span class="style26">QUOTES</span></div></td>
+  </tr>
+  <tr>
+    <td height="208" width="220" background="Untitled - Copy.png">
+    <p> <center><img src="a.p.j.jpg" width="212" height="174">“</center></p>
+      <p align="justify">“God, our Creator, has stored within our minds and personalities, great potential strength and ability. Prayer helps us tap and develop these powers.”</p>
+      <p class="style25">-A.P.J.ABDUL KALAM
+      <p align="justify"><img src="Bill-Gates-The-story-of-the-richest-man-in-the-world.jpg" alt="bg" width="219" height="182" />
+        &quot;People always fear change. People feared electricity when it was invented, didn't they? People feared coal, they feared gas-powered engines... There will always be ignorance, and ignorance leads to fear. But with time, people will come to accept their silicon masters.&quot; </p>
+      <p><br />
+          <span class="style25"><a href="http://www.brainyquote.com/quotes/quotes/b/billgates191270.html" class="style25">-BILL GATES</a> </span> </p>
+      <p>
+        <center>
+          <img src="azim premji.jpg" alt="az" width="215" height="164" />
+        </center>
+         
+      </p>
+      <p align="justify"> “We believe this combination of excellence in operations and strong execution of our strategy is critical to achieve our vision. We will continue to focus on both in future as well.” </p>
+    <p class="style25"><u>-AZIM PREMJI</u></p></td>
+  </tr>
+  <tr>
+    <td height="49" colspan="2" align="center" background="img04.jpg"><p class="style3"><span class="style1">Copyright &copy;&nbsp;</span><span class="style5">2011 &nbsp;  iMinds Group</span></p>       </td>
+  </tr>
+</table>
+
+
+<script type="text/javascript">
+<!--
+var MenuBar1 = new Spry.Widget.MenuBar("MenuBar1", {imgDown:"SpryMenuBarDownHover.gif", imgRight:"SpryMenuBarRightHover.gif"});
+//-->
+</script>
+ </form> 
+</body>
+</html>
