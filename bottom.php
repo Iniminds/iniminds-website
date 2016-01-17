0db18e0f8ffacab4a6aaf35fb0225edc46d53c89
Archit Trivedi <archittrivediui@gmail.com>
2016-01-16 20:42:03 -0800

    bottom.php, c.php
    Two more files added, bottom.php and c. php

---
+++ bottom.php
@@ -0,0 +1,11 @@
+<style type="text/css">
+<!--
+.style1 {color: #FFFFFF}
+-->
+</style>
+
+
+
+<table width="1107"><td width="1107" height="49" align="center" background="images/img04.jpg" valign="top"><p class="style3 style1">
+<span class="style1">Copyright &copy;&nbsp;</span><span class="style5">2011 &nbsp;  iMinds Group</span></p>   
+</td></table>
---
+++ c.php
@@ -0,0 +1,190 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>iMinds C/C++</title>
+<script src="SpryMenuBar.js" type="text/javascript"></script>
+
+<link href="SpryMenuBarHorizontal.css" rel="stylesheet" type="text/css" />
+<style type="text/css">
+<!--
+.style3 {
+	color: #000000;
+	font-weight: bold;
+	font-size: 24px;
+}
+.style5 {color: #999999}
+.style6 {font-weight: bold}
+.style7 {font-size: 18px}
+.style8 {font-size: 24px}
+.style10 {
+	font-size: 36px;
+	color: #330066;
+}
+.style19 {color: #000066}
+.style20 {font-size: 22px}
+.style25 {	color: #0000FF;
+	font-weight: bold;
+}
+.style26 {color: #CCCCCC; font-weight: bold; }
+.style28 {color: #FFFFFF}
+-->
+</style>
+<script type="text/javascript">
+function get()
+{
+	document.getElementById('q').value=' ';
+}
+function MM_swapImgRestore() { //v3.0
+  var i,x,a=document.MM_sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
+}
+</script>
+</head>
+<body background="img07.jpg">
+ <form action="http://www.google.com/search" target="_blank" method="get"> 
+<table width="1184" border="0" align="center"  bgcolor="#999999">
+  <tr>
+    <td height="225" colspan="8" ><img src="headerimg.jpg"  height="106%" width="100%"/>           </td>
+  </tr>
+  <tr>
+  <td height="25" colspan="2"  background="menuimg.png"> 
+  <ul id="MenuBar1" class="MenuBarHorizontal">
+  <li><a class="MenuBarItemSubmenu" href="index.php">HOME</a>
+  <li><a href="#" class="MenuBarItemSubmenu style6">REFERENCES</a>
+      <ul>
+        <li><a href="c.php">C/C++</a></li>
+        <li><a href="java.php">JAVA</a></li>
+        <li><a href="python.php">PYTHON</a></li>
+        <li><a href="linux.php">LINUX</a></li>
+      </ul>
+  </li>
+<li><a class="MenuBarItemSubmenu" href="#">OFFTOPIC</a>
+            <ul>
+              <li><a href="tech.php">TECH</a></li>
+              <li><a href="nontech.php">NON-TECH</a></li>
+            </ul>
+      <li><a class="MenuBarItemSubmenu" href="genesis.php">GENESIS</a>
+           <li><a class="MenuBarItemSubmenu" href="feedback.php">FEEDBACK</a></li>
+       <li><a class="MenuBarItemSubmenu" href="#">ABOUT US</a>
+       <ul>
+        <li><a href="members.php">MEMBERS</a></li>
+        <li><a href="contact.php">CONTACT</a></li>
+      </ul>
+</ul>
+
+  <div align="right">
+    <script type="text/javascript">
+<!--
+var MenuBar1 = new Spry.Widget.MenuBar("MenuBar1", {imgDown:"SpryMenuBarDownHover.gif", imgRight:"SpryMenuBarRightHover.gif"});
+//-->
+   </script>
+    
+    <input type="text"  name="q" id="q" style="width: 120px; color:#999999" onFocus="get();" value="Google" />
+    <input type="submit" value="Search" /> 
+  </div></td>
+  </tr>
+  <tr   bordercolor="#FFFFDF" >
+    <td width="954" rowspan="4" valign="top" bgcolor="#ECECFF"><div align="center" class="style7"><a href="#" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('Image7','','c++.jpg',1)"><img src="c.jpg" name="Image7" width="160" height="204" border="0" id="Image7" /></a>
+        <p align="justify"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="style8">C is a general-purpose computer programming language 
+developed between 1969 and 1973 by Dennis Ritchie 
+at the Bell Telephone Laboratories.Although C was designed 
+for implementing system software,it is also
+ widely used for developing portable application 
+software.C is one of the most widely used 
+programming languages of all time. C has greatly 
+influenced many other popular programming
+ languages, most notably C++, which began as an 
+extension to C. </span></p>
+        <div align="left">
+          <ul>
+            <div align="center"><span class="style10">C/C++ Programs</span>            </div>
+            <li> 
+              <div align="justify"><span class="style8"><span class="style20"><span class="style8"><a href="variable.pdf" class="style8">Constant</a></span></span></span></div>
+            </li>
+            <li class="style8">
+              <div align="justify"><a href="looping.pdf">Decision Making and Looping</a></div>
+            </li>
+            <li class="style8">
+              <div align="justify">Arrays:- 1) <a href="sort an array.pdf">sorting an array</a></div>
+            </li>
+            <li class="style8">
+              <div align="justify">User Defined Functions</div>
+            </li>
+            <li class="style8">
+              <div align="justify">Pointers:- 1) <a href="pointer.pdf">pointer</a></div>
+            </li>
+            <li class="style8">
+              <div align="justify">Stack:-  1<a href="stack.pdf">)simple stack</a> 2) <a href="stack using array.pdf">stack using array</a> 3)<a href="stackdata structure.pdf">stack using structure</a></div>
+            </li>
+            <li class="style8">
+              <div align="justify">Queue:-  1)<a href="queue.pdf"> simple queue</a> 2)<a href="queue using linkedlist.pdf"> queue using linklist</a>   3) <a href="queue using array.pdf">queue using array</a>4)<a href="circular queue.pdf"> circular queue            </a></div>
+            </li>
+            <li class="style8">
+              <div align="justify">Linklist:- 1)<a href="circular linklist.pdf"> circular linklist</a> 2)<a href="single linkedlist.pdf"> singly linklist</a> 3) <a href="doubled linklist.pdf">doubly linklist</a></div>
+            </li>
+            <li class="style8">
+              <div align="justify">Tree:- 1) <a href="implement binary search.pdf">binary search tree</a></div>
+            </li>
+            <li class="style8">
+              <div align="justify">Sorting :- 1)<a href="insertionsort.pdf"> insertion sort</a> 2)<a href="mergesort.pdf"> merge sort</a> 3) <a href="shellsort.pdf">shell sort </a> 4) <a href="bubblesort.pdf">bubble sort</a> 5) <a href="heapsort.pdf">heap sort</a></div>
+            </li>
+            <li class="style8">
+              <div align="justify">Inheritance</div>
+            </li>
+            <li class="style8">
+              <div align="justify">Polymorphism</div>
+            </li>
+            <li>
+              <div align="justify"><span class="style8">Encapsulation </span></div>
+            </li>
+          </ul>
+        </div>
+    </div></td>
+    <td width="220"  height="25" background="box_h3.png"><div align="center"><span class="style26">Recent Updates</span> </div></td>
+  </tr>
+  <tr>
+    <td height="135" background="Untitled - Copy.png"><MARQUEE  onmouseover=this.stop() onmouseout=this.start() scrollAmount=2 scrollDelay=0 direction="up" >
+    <div align="justify"><span class="style19">
+      <p class="style19"><img src="new.gif" alt="new1" /> Python Workshop @ ADIT organized by Computer Engg Department under ISTE Student Chapter. <br>
+          <br>
+          <img src="new.gif" alt="new" />Solved C/C++ &amp; JAVA Programs. </p>
+      <p align="left" class="style19"><img src="new.gif" alt="new" /> Coming Soon:-UML.SQLqueries and more programs</p>
+    </div>
+    </marquee>
+      </td>
+  </tr>
+  <tr>
+    <td height="25"  background="box_h3.png"><div align="center"><span class="style26">QUOTES</span></div></td>
+  </tr>
+  <tr>
+    <td height="250" width="220" background="Untitled - Copy.png">
+     <p> <center><img src="a.p.j.jpg" width="212" height="174">“</center></p>
+      <p align="justify">“God, our Creator, has stored within our minds and personalities, great potential strength and ability. Prayer helps us tap and develop these powers.”</p>
+      <p class="style25">-A.P.J.ABDUL KALAM
+      <p align="justify"><img src="Bill-Gates-The-story-of-the-richest-man-in-the-world.jpg" alt="bg" width="219" height="182" />&nbsp;&quot;People always fear change. People feared electricity when it was invented, didn't they? People feared coal, they feared gas-powered engines... There will always be ignorance, and ignorance leads to fear. But with time, people will come to accept their silicon masters.&quot; </p>
+      <p><br />
+          <span class="style25">-BILL GATES</span></p>
+      <p>
+        <center>
+          <img src="azim premji.jpg" alt="az" width="215" height="164" />
+        </center>
+         
+      </p>
+      <p align="justify"> “We believe this combination of excellence in operations and strong execution of our strategy is critical to achieve our vision. We will continue to focus on both in future as well.” </p>
+      <p class="style25">-AZIM PREMJI</p>
+    </marquee></td>
+  </tr>
+  <tr>
+    <td height="49" colspan="2" align="center" background="img04.jpg"><p class="style3"><span class="style1">Copyright &copy;&nbsp;</span><span class="style5">2011 &nbsp;  iMinds Group</span></p>     </td>
+  </tr>
+</table>
+
+
+<script type="text/javascript">
+<!--
+var MenuBar1 = new Spry.Widget.MenuBar("MenuBar1", {imgDown:"SpryMenuBarDownHover.gif", imgRight:"SpryMenuBarRightHover.gif"});
+//-->
+</script>
+</form>
+</body>
+</html>
